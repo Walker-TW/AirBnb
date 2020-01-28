2e766f1f0004bee40b3266f7c87da29cbccf8583
ria patel <ria.amy.patel@gmail.com>
2020-01-28 11:31:03 +0000

    prelim work on views

--- app.rb
+++ app.rb
@@ -2,11 +2,42 @@ require 'sinatra/base'
 # require './lib/bookmarks'
 
 class MakersBnB < Sinatra::Base
-
   enable :sessions
 
   get '/' do 
-    "Helo World"
+    erb :index
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    session[:username] = params[:username]
+    session[:password]= params[:password]
+    redirect to '/portal'
+  end
+
+  post '/portal' do
+    @username = session[:username]
+    @password = session[:password]
+    erb :portal
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:username] = params[:username]
+    session[:password]= params[:password]
+    redirect to '/congrats'
+  end
+
+  post '/congrats' do
+    @username = session[:username]
+    @password = session[:password]
+    erb :congrats
   end
 
   run! if app_file == $0
