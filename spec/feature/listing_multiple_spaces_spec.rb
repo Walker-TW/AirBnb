fe56507bdbd05349377cc42bc2a9e13dbc9eaf90
Squareben1 <bengittins@live.com>
2020-01-30 11:54:52 +0000

    fixed tests, all passing - can list multiple properties

--- spec/feature/listing_multiple_spaces_spec.rb
+++ spec/feature/listing_multiple_spaces_spec.rb
@@ -1,26 +1,30 @@
 feature 'list space' do
   scenario 'user can list multiple spaces' do
-    visit '/' 
+    user = User.create(user_name: "Ben G", password: "password456")
+    visit '/login/new'
+    fill_in :user_name, with: 'Ben G'
+    fill_in :password, with: 'password456'
     click_on 'Login'
-    fill_in 'username', with: 'toadymctoadface'
-    fill_in 'password', with: 'ribbit'
-    click_on 'enter'
-    click_on 'create a new listing'
-    fill_in 'name', with: 'name'
-    fill_in 'description', with: 'description'
-    fill_in 'ppn', with: 100
-    click_on 'enter'
-    expect(page).to have_content('your listing has been added')
-    visit '/' 
-    click_on 'Login'
-    fill_in 'username', with: 'toadymctoadface'
-    fill_in 'password', with: 'ribbit'
-    click_on 'enter'
-    click_on 'create a new listing'
-    fill_in 'name', with: 'name2'
-    fill_in 'description', with: 'description2'
-    fill_in 'ppn', with: 101
-    click_on 'enter'
-    expect(page).to have_content('your listing has been added')
+    click_on 'List a New Property'
+    fill_in :space_name, with: 'Test Property'
+    fill_in :city, with: 'London'
+    fill_in :description, with: 'test description'
+    fill_in :ppn, with: '100'
+    click_on 'Post'
+    click_on 'List of Properties'
+    expect(page).to have_content('Name: Test Property')
+    expect(page).to have_content('City: London')
+    expect(page).to have_content('Description: test description')
+    click_on 'Take Me Home Country Road'
+    click_on 'List a New Property'
+    fill_in :space_name, with: 'Test Property 2'
+    fill_in :city, with: 'Edinburgh'
+    fill_in :description, with: 'test description 2'
+    fill_in :ppn, with: '120'
+    click_on 'Post'
+    click_on 'List of Properties'
+    expect(page).to have_content('Name: Test Property 2')
+    expect(page).to have_content('City: Edinburgh')
+    expect(page).to have_content('Description: test description 2')
   end
 end
