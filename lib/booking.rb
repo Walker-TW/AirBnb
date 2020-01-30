b55fe6c943a056eb32e62e1fe57d6434082d42eb
Squareben1 <bengittins@live.com>
2020-01-30 18:13:45 +0000

    Booking function added and tested

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,16 +1,16 @@
 class Booking 
   
-  attr_reader :space_name, :date, :user_id#, :renter_id
+  attr_reader :bookings_id, :space_id, :date, :user_id#, :renter_id
 
-  def initialize(space_name:, date:, user_id:)#, renter_id: )
-    @space_name = space_name
+  def initialize(bookings_id:, space_id:, date:, user_id:)#, renter_id: )
+    @space_id = space_id
     @date = date
     @user_id = user_id
     # @renter_id = renter_id
   end
 
-  def self.create(space_name:, date:, user_id:)#, renter_id: )
-    result = DatabaseConnection.query("INSERT INTO bookings (space_name, date, user_id) VALUES ('#{space_name}', '#{city}', '#{description}', '#{ppn}') RETURNING space_name, city, description, ppn")
-    Booking.new(space_name: result[0]['space_name'], city: result[0]['city'], description: result[0]['description'], ppn: result[0]['ppn'])
+  def self.create(space_id:, date:, user_id:)#, renter_id: )
+    result = DatabaseConnection.query("INSERT INTO bookings (space_id, user_id, date) VALUES ('#{space_id}', '#{user_id}', '#{date}') RETURNING bookings_id, space_id, user_id, date")
+    Booking.new(bookings_id: result[0]['bookings_id'], space_id: result[0]['space_id'], user_id: result[0]['user_id'], date: result[0]['date'])
   end
 end
--- lib/space.rb
+++ lib/space.rb
@@ -1,9 +1,10 @@
 require 'pg'
 
 class Space
-  attr_reader :space_name, :city, :description, :ppn
+  attr_reader :space_id, :space_name, :city, :description, :ppn
 
-  def initialize(space_name:, city:, description:, ppn:)
+  def initialize(space_id:, space_name:, city:, description:, ppn:)
+    @space_id = space_id
     @space_name = space_name
     @city = city
     @description = description
@@ -12,19 +13,19 @@ class Space
 
   def self.find(space_name:)
     result = DatabaseConnection.query("SELECT * FROM spaces WHERE space_name = '#{space_name}'")
-    Space.new(space_name: result[0]['space_name'], city: result[0]['city'], description: result[0]['description'], ppn: result[0]['ppn'])
+    Space.new(space_id: result[0]['space_id'], space_name: result[0]['space_name'], city: result[0]['city'], description: result[0]['description'], ppn: result[0]['ppn'])
   end
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM spaces")
     result.map do |space|
-      Space.new(space_name: space['space_name'], city: space['city'], description: space['description'], ppn: space['ppn'])
+      Space.new(space_id: space['space_id'], space_name: space['space_name'], city: space['city'], description: space['description'], ppn: space['ppn'])
     end
   end
 
   def self.create(space_name:, city:, description:, ppn:)
-    result = DatabaseConnection.query("INSERT INTO spaces (space_name, city, description, ppn) VALUES ('#{space_name}', '#{city}', '#{description}', '#{ppn}') RETURNING space_name, city, description, ppn")
-    Space.new(space_name: result[0]['space_name'], city: result[0]['city'], description: result[0]['description'], ppn: result[0]['ppn'])
+    result = DatabaseConnection.query("INSERT INTO spaces (space_name, city, description, ppn) VALUES ('#{space_name}', '#{city}', '#{description}', '#{ppn}') RETURNING space_id, space_name, city, description, ppn")
+    Space.new(space_id: result[0]['space_id'], space_name: result[0]['space_name'], city: result[0]['city'], description: result[0]['description'], ppn: result[0]['ppn'])
   end
 
 end
